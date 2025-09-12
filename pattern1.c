6f1a8b2a5cbd4e1f7c7642570a1276b6f50bfc82
Avni <thapliyal.avni2006@gmail.com>
2025-09-12 19:50:55 +0530

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int r;
+	printf("enter rows");
+	scanf("%d", &r);
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
